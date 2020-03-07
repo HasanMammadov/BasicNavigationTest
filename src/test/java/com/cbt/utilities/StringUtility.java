de525e9366eaaaec2324806656e9388058175da7
HasanMammadov <hasanmamedoff@gmail.com>
2020-03-07 01:45:36 -0500

    String Utility

--- src/test/java/com/cbt/utilities/StringUtility.java
+++ src/test/java/com/cbt/utilities/StringUtility.java
@@ -9,12 +9,15 @@ public class StringUtility {
 
     public static void verifyEquals(String expected,String actual){
 
-        System.out.println("expected = " + expected);
-        System.out.println("actual = " + actual);
-        if(expected.equals(actual)){
 
+        if(expected.equals(actual)){
+            System.out.println("expected = " + expected);
+            System.out.println("actual = " + actual);
             System.out.println("PASS");
         }else {
+
+            System.out.println("expected = " + expected);
+            System.out.println("actual = " + actual);
             System.out.println("FAIL");
         }
 
