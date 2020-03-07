e64237aa310a5ae6544c40dd2a944cee09ed3a38
HasanMammadov <hasanmamedoff@gmail.com>
2020-03-07 03:07:26 -0500

    TitleVerification3

---
+++ src/test/java/com/cbt/utilities/TitleVerification3.java
@@ -0,0 +1,34 @@
+package com.cbt.utilities;
+
+import org.openqa.selenium.WebDriver;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TitleVerification3 {
+    public static void main(String[] args) {
+        List<String> urls = Arrays.asList("http://luluandgeorgia.com","http://wayfair.com","http://walmart.com",
+                "http://westelm.com");
+        WebDriver driver = BrowserFactory.getDriver("chrome");
+        for (String url:urls) {
+
+
+            driver.get(url);
+            driver.manage().window().maximize();
+            String title = driver.getTitle().replace(" ","").toLowerCase();
+
+            if(driver.getCurrentUrl().contains(title)){
+                System.out.println("Pass - "+driver.getCurrentUrl()+" url contains "+ title);
+            }else{
+                System.out.println("Fail - "+driver.getCurrentUrl()+" does NOT  contains "+ title);
+
+            }
+
+        }
+
+        driver.quit();
+
+
+
+    }
+}
