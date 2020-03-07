ced32e11553f5d8c51917a5c437cdc4acbbf153a
HasanMammadov <hasanmamedoff@gmail.com>
2020-03-06 19:29:40 -0500

    This is a new commit for what I originally planned to be amended

---
+++ src/test/java/com/cbt/utilities/BrowserFactory.java
@@ -0,0 +1,18 @@
+package com.cbt.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserFactory {
+    public static void main(String[] args) {
+
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://google.com");
+        driver.quit();
+
+    }
+}
