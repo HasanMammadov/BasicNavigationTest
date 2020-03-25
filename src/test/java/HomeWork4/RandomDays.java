a518bdec039dee7be5bb6a396f363acb04757afd
HasanMammadov <hasanmamedoff@gmail.com>
2020-03-25 01:26:20 -0400

    HomeWork4, task1. Randomly check box

--- src/test/java/HomeWork4/RandomDays.java
+++ src/test/java/HomeWork4/RandomDays.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -16,81 +17,10 @@ import java.util.concurrent.TimeUnit;
 
 public class RandomDays {
 
-
-    @Test
-    public void weekDays() throws InterruptedException {
-   // driver.get("http://samples.gwtproject.org/samples/Showcase/Showcase.html#!CwCheckBox");
-//        WebElement checkBox = driver.findElement(By.xpath("//table//tbody//tr[2]//input"));
-//        WebElement weekDay = driver.findElement(By.xpath("//table//tbody//tr[2]"));
-//        //2-6
-
-        int countFriday = 0;
-
-  //while (countFriday>=3) {
-    Random random = new Random();
-    int num = random.nextInt(5);
-        Thread.sleep(5000);
-
-        List<WebElement> days = driver.findElements(By.xpath("//table//tbody//tr[" + (num + 2) + "]"));
-
-    String wDays ="";
-    for (WebElement each : days) {
-        wDays=each.getText();
-
-        each.click();
-        System.out.println(each.getText());
-        each.click();
-
-    }
-
-
-
-//    if(wDays.equals("Friday")){
-//        countFriday++;
-//    }
-//}
-
-
-
-
-
-
-
-
-
-//        Random random = new Random();
-//            int num = random.nextInt(5);
-//            Thread.sleep(3000);
-//             driver.findElement(By.xpath("//table//tbody//tr["+(num+2)+"]//input")).click();
-//            Thread.sleep(3000);
-//
-//            WebElement weekDay = driver.findElement(By.xpath("//table//tbody//tr["+(num+2)+"]"));
-//            Thread.sleep(3000);
-//
-//            String eachDay = weekDay.getText();
-//            Thread.sleep(3000);
-//
-//            System.out.println(eachDay);
-//            Thread.sleep(3000);
-//
-//            driver.findElement(By.xpath("//table//tbody//tr["+(num+2)+"]//input")).click();
-//            Thread.sleep(3000);
-//
-//            if(eachDay.equals("Friday")){
-//                countFriday++;
-//            }
-//
-//
-      }
-
-
-
-
-
-
     private WebDriver driver;
+
     @BeforeMethod
-    public void setDriver(){
+    public void setDriver() {
         WebDriverManager.chromedriver().version("79").setup();
         driver = new ChromeDriver();
         driver.get("http://samples.gwtproject.org/samples/Showcase/Showcase.html#!CwCheckBox");
@@ -99,7 +29,38 @@ public class RandomDays {
     }
 
     @AfterMethod
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();
+    }
+
+    @Test
+    public void weekDays() throws InterruptedException {
+
+        List<WebElement> weekDays = driver.findElements(By.xpath("//label"));
+        List<WebElement> checkBoxes = driver.findElements(By.cssSelector("[type='checkbox']"));
+
+        Random random = new Random();
+
+        int fridayCount=0;
+
+        while (fridayCount!=3){
+            int index = random.nextInt(7);
+
+            if(checkBoxes.get(index).isEnabled()){
+                checkBoxes.get(index).click();
+                String days = weekDays.get(index).getText();
+                System.out.println(days);
+                checkBoxes.get(index).click();
+            }
+
+            if(weekDays.get(index).getText().equals("Friday")){
+                fridayCount++;
+            }
+        }
+
+
+        Assert.assertTrue(fridayCount==3);
+
+
     }
 }
